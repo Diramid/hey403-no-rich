80d5934a7b32c35c9804c5276df12970f9e4619e
Mohammad222PR <mohammades13851@gmail.com>
2025-02-01 02:10:24 +0330

    refactor(cli): Split `set_dns` logic into `configure_linux_dns`, `configure_windows_dns`, and `configure_mac_dns`

--- hey403/utils/dns_utils.py
+++ hey403/utils/dns_utils.py
@@ -1,6 +1,8 @@
 import ctypes
 import logging
+import os
 import subprocess
+import sys
 
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -94,8 +96,92 @@ def configure_dns(connection: str, dns_servers: str) -> bool:
     return True
 
 
-def is_admin():
+def configure_linux_dns(preferred_dns: str, alternative_dns: str | None) -> None:
+    if os.geteuid() != 0:
+        logging.error("Please run with sudo!")
+        sys.exit(1)
+
+    active_connections = get_active_connections()
+    if not active_connections:
+        logging.error("No active network connections found.")
+        sys.exit(1)
+
+    active_connection = active_connections[0]
+    dns_servers = f"{preferred_dns} {alternative_dns}" if alternative_dns else preferred_dns
+
+    if not configure_dns(connection=active_connection, dns_servers=dns_servers):
+        logging.error(f"Failed to configure DNS({dns_servers}) on connection {active_connection}")
+        sys.exit(1)
+
+    logging.info("DNS successfully set for connection: %s!", active_connection)
+
+
+def configure_windows_dns(preferred_dns: str, alternative_dns: str | None) -> None:
+    if not is_admin():
+        logging.error("Please run as Administrator!")
+        logging.warning("You can run cmd or power shell as Administrator!")
+        sys.exit(1)
+
+    interface = get_activate_interface()
+    if not interface:
+        logging.error("No active interface found!")
+        sys.exit(1)
+
+    interface_name = interface[0]
     try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
+        subprocess.run(
+            f'netsh interface ip set dns "{interface_name}" static {preferred_dns} primary',
+            shell=True,
+            check=True,
+        )
+
+        if alternative_dns:
+            subprocess.run(
+                f'netsh interface ip add dns "{interface_name}" {alternative_dns} index=2',
+                shell=True,
+                check=True,
+            )
+
+        logging.info("DNS successfully set!")
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Error setting DNS: {e}")
+        sys.exit(1)
+
+
+def configure_mac_dns(preferred_dns: str, alternative_dns: str | None) -> None:
+    try:
+        connection_type = None
+        for interface in ["Wi-Fi", "Ethernet"]:
+            try:
+                subprocess.run(
+                    f"networksetup -getdnsservers {interface}",
+                    shell=True,
+                    check=True,
+                    stdout=subprocess.PIPE,
+                )
+                connection_type = interface
+                break
+            except subprocess.CalledProcessError:
+                continue
+
+        if not connection_type:
+            logging.error("No active network interfaces (Wi-Fi or Ethernet) found!")
+            sys.exit(1)
+
+        base_command = f"networksetup -setdnsservers {connection_type}"
+        dns_servers = f"{preferred_dns} {alternative_dns}" if alternative_dns else preferred_dns
+
+        subprocess.run(
+            f"{base_command} {dns_servers}",
+            shell=True,
+            check=True,
+        )
+
+        logging.info("DNS successfully set for %s!", connection_type)
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Error while setting DNS: {e}")
+        sys.exit(1)
+
+
+def is_admin():
+    return ctypes.windll.shell32.IsUserAnAdmin()
