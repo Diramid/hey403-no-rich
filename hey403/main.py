f4f8623c66b7d0dc0891f066ccbada9dcd59f492
Mohammad222PR <mohammades13851@gmail.com>
2025-02-26 16:52:43 +0330

    fix(cli): solving absolute import problem

--- hey403/main.py
+++ hey403/main.py
@@ -3,7 +3,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from rich.console import Console
 from rich.progress import Progress
 
-from cli.parser import build_parser
+from hey403.cli.parser import build_parser
 from hey403.services.dns_resolver import set_dns, test_dns
 from hey403.utils.network_utils import check_internet_connection
 from hey403.utils.table import create_table
@@ -11,7 +11,6 @@ from network.dns_servers import DNS_SERVERS
 
 
 def main():
-
     check_internet_connection()
 
     parser = build_parser()
